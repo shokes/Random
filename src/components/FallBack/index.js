4977f5b64dd4b17c1cc86d414ad118e77a255bf7
Oshoke Oyati <oyatioshoke@yahoo.com>
2022-11-04 14:45:36 +0100

    error boundary

---
+++ src/components/FallBack/index.js
@@ -0,0 +1,14 @@
+const OurFallbackComponent = ({
+  error,
+  componentStack,
+  resetErrorBoundary,
+}) => {
+  return (
+    <div>
+      <h1>An error occurred: {error.message}</h1>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+};
+
+export default OurFallbackComponent;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import Loading from '../Loading';
+import ErrorBoundary from '../ErrorBoundary';
+import OurFallbackComponent from '../FallBack';
 
 const Home = () => {
   const [users, setUsers] = useState([]);
@@ -44,74 +46,76 @@ const Home = () => {
     );
   } else {
     return (
-      <section className='container'>
-        <div className='grid-users'>
-          {users.map((user) => {
-            const {
-              gender,
-              email,
-              phone,
-              dob: { age },
-              name: { first, last },
-              picture: { large: image },
-            } = user;
+      <ErrorBoundary FallbackComponent={OurFallbackComponent}>
+        <section className='container'>
+          <div className='grid-users'>
+            {users.map((user) => {
+              const {
+                gender,
+                email,
+                phone,
+                dob: { age },
+                name: { first, last },
+                picture: { large: image },
+              } = user;
 
-            return (
-              <div className='card' key={email}>
-                <img src={image} alt={first} className='image' />
-                <div>
-                  <span>
-                    {first} {last}
-                  </span>
-                  <div>{gender}</div>
-                  <div>{age}</div>
-                  <div>{email}</div>
-                  <div>{phone}</div>
+              return (
+                <div className='card' key={email}>
+                  <img src={image} alt={first} className='image' />
+                  <div>
+                    <span>
+                      {first} {last}
+                    </span>
+                    <div>{gender}</div>
+                    <div>{age}</div>
+                    <div>{email}</div>
+                    <div>{phone}</div>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className='functions'>
-          <button onClick={handlePrev} className='button'>
-            Prev
-          </button>
-          <span
-            onClick={() => setNumber(1)}
-            className={`number ${number === 1 ? 'active' : null}`}
-          >
-            1
-          </span>
-          <span
-            onClick={() => setNumber(2)}
-            className={`number ${number === 2 ? 'active' : null}`}
-          >
-            2
-          </span>
-          <span
-            onClick={() => setNumber(3)}
-            className={`number ${number === 3 ? 'active' : null}`}
-          >
-            3
-          </span>
-          <span
-            onClick={() => setNumber(4)}
-            className={`number ${number === 4 ? 'active' : null}`}
-          >
-            4
-          </span>
-          <span
-            onClick={() => setNumber(5)}
-            className={`number ${number === 5 ? 'active' : null}`}
-          >
-            5
-          </span>
+              );
+            })}
+          </div>
+          <div className='functions'>
+            <button onClick={handlePrev} className='button'>
+              Prev
+            </button>
+            <span
+              onClick={() => setNumber(1)}
+              className={`number ${number === 1 ? 'active' : null}`}
+            >
+              1
+            </span>
+            <span
+              onClick={() => setNumber(2)}
+              className={`number ${number === 2 ? 'active' : null}`}
+            >
+              2
+            </span>
+            <span
+              onClick={() => setNumber(3)}
+              className={`number ${number === 3 ? 'active' : null}`}
+            >
+              3
+            </span>
+            <span
+              onClick={() => setNumber(4)}
+              className={`number ${number === 4 ? 'active' : null}`}
+            >
+              4
+            </span>
+            <span
+              onClick={() => setNumber(5)}
+              className={`number ${number === 5 ? 'active' : null}`}
+            >
+              5
+            </span>
 
-          <button onClick={handleNext} className='button'>
-            Next
-          </button>
-        </div>
-      </section>
+            <button onClick={handleNext} className='button'>
+              Next
+            </button>
+          </div>
+        </section>
+      </ErrorBoundary>
     );
   }
 };
